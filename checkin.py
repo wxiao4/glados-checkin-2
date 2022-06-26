60d71b7753403b33368029387b8b68107a50e3ac
wxiao4 <32830154+wxiao4@users.noreply.github.com>
2022-06-26 17:11:21 +0800

    Create checkin.py

---
+++ checkin.py
@@ -0,0 +1,44 @@
+import requests,json,os
+
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever = os.environ["SERVE"]
+
+# 填写server酱sckey,不开启server酱则不用填
+sckey = os.environ["SCKEY"]
+
+# 填入glados账号对应cookie
+cookie = os.environ["COOKIE"]
+
+
+def start():    
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    #checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    #state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+    origin = "https://glados.rocks"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    payload={ 
+        # 'token': 'glados_network'
+        'token': 'glados.network'
+    }
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+   # print(res)
+
+
+    if 'message' in checkin.text:
+        mess = checkin.json()['message']
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        print(time)
+        if sever == 'on':
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+    else:
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+    start()
